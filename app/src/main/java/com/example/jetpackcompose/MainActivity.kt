b36b826032e1a6aae8feea628302468b55f61114
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-29 11:11:20 +0000

    Text Composable vs Custom Modifier

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +38,18 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = Color.Blue,
+        fontSize = 32.sp,
+        // Text Composable vs Custom Modifier
+        // The above properties are for the Text composable
+        // color, fontSize, and text are properties of the Text composable itself
+        // modifier is not a property of the Text composable, but it is used to modify the appearance of the Text composable
+
+        // modifier = modifier.padding(16.dp).background(Color.Yellow)
+        // ORDER is important, the code above is different from the code below
         modifier = modifier
+            .background(Color.Yellow)
+            .padding(16.dp)
     )
 }
 
