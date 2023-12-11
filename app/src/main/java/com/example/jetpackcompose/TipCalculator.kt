79f49201cfa682a21e3ac5620b6a2d0ceeca7080
Musa <79163385+musayuksel@users.noreply.github.com>
2023-12-11 15:11:15 +0000

    Add action button functionality

--- app/src/main/java/com/example/jetpackcompose/TipCalculator.kt
+++ app/src/main/java/com/example/jetpackcompose/TipCalculator.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,6 +66,9 @@ fun TipCalculator() {
         )
         EditNumberField(
             label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number, imeAction = ImeAction.Done
+            ),
             amountInput = tipPercent,
             onValueChange = { tipPercent = it },
             modifier = Modifier
@@ -92,7 +96,10 @@ fun EditNumberField(
     amountInput: String,
     onValueChange: (String) -> Unit,
     @StringRes label: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(
+        keyboardType = KeyboardType.Number, imeAction = ImeAction.Next
+    ),
 ) {
 
     TextField(
@@ -100,7 +107,7 @@ fun EditNumberField(
         onValueChange = onValueChange,
         label = { Text(stringResource(label)) },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = modifier
     )
 }
