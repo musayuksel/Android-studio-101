62fb2c3f25f3c221c0b681289b6c1677c202daaf
Musa <79163385+musayuksel@users.noreply.github.com>
2023-12-11 15:48:03 +0000

    Add icon for inputs

--- app/src/main/java/com/example/jetpackcompose/TipCalculator.kt
+++ app/src/main/java/com/example/jetpackcompose/TipCalculator.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcompose
 
 import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -27,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -66,6 +69,7 @@ fun TipCalculator() {
             amountInput = amountInput,
             onValueChange = { amountInput = it },
             label = R.string.bill_amount,
+            leadingIcon = R.drawable.money_icon,
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
@@ -76,6 +80,7 @@ fun TipCalculator() {
                 keyboardType = KeyboardType.Number, imeAction = ImeAction.Done
             ),
             amountInput = tipPercent,
+            leadingIcon = R.drawable.percent_icon,
             onValueChange = { tipPercent = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
@@ -108,6 +113,7 @@ fun EditNumberField(
     amountInput: String,
     onValueChange: (String) -> Unit,
     @StringRes label: Int,
+    @DrawableRes leadingIcon: Int,
     modifier: Modifier = Modifier,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(
         keyboardType = KeyboardType.Number, imeAction = ImeAction.Next
@@ -118,6 +124,7 @@ fun EditNumberField(
         value = amountInput,
         onValueChange = onValueChange,
         label = { Text(stringResource(label)) },
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
         singleLine = true,
         keyboardOptions = keyboardOptions,
         modifier = modifier
