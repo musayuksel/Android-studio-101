5aefe544aaa2fea4e881dd06833ea6fcb06c6b27
Musa <79163385+musayuksel@users.noreply.github.com>
2023-12-14 16:29:38 +0000

    Add styles for card

--- app/src/main/java/com/example/jetpackcompose/ArtSpaceExercise.kt
+++ app/src/main/java/com/example/jetpackcompose/ArtSpaceExercise.kt
@@ -1,18 +1,30 @@
 package com.example.jetpackcompose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 data class Art(
     val id: Int,
@@ -70,18 +82,21 @@ fun ArtSpaceExercise(modifier: Modifier = Modifier) {
 
 
 
-    Column {
-//        image
+    Column (
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
         ArtImage(
             imageResource = exampleImages[currentImageIndex].imageResource,
             description = exampleImages[currentImageIndex].description,
-            modifier = modifier
+            modifier = modifier.weight(3f)
         )
         ArtExplanation(
             title = exampleImages[currentImageIndex].title,
             artist = exampleImages[currentImageIndex].artist,
             year = exampleImages[currentImageIndex].year,
-            modifier = modifier
+            modifier = modifier.padding(horizontal = 24.dp)
         )
 
         NavigationImages(
@@ -91,18 +106,29 @@ fun ArtSpaceExercise(modifier: Modifier = Modifier) {
             handlePreviousClick = {
                 handlePreviousImageClick()
             },
-            modifier = modifier
+            modifier = modifier.weight(1f)
         )
     }
 }
 
 @Composable
 fun ArtImage(imageResource: Int, description: String, modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(imageResource),
-        contentDescription = description,
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier.fillMaxSize().padding(16.dp)
+            .shadow(14.dp, shape = RoundedCornerShape(8.dp))
+    ) {
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            modifier = modifier
+
+        ) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = description,
+                modifier = Modifier.fillMaxSize().padding(16.dp)
+            )
+        }
+    }
 }
 
 @Composable
@@ -112,8 +138,12 @@ fun ArtExplanation(
     year: Int,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(text = title)
+    Column(modifier = modifier.fillMaxWidth()
+        .background(Color.Magenta).padding(vertical = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = title, fontSize = 24.sp)
         Row {
             Text(text = artist)
             Text(text = year.toString())
