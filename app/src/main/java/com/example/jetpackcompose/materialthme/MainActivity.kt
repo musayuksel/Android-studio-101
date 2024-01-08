f7565a9c41391c69363fa628acc11a50ced0808b
Musa <79163385+musayuksel@users.noreply.github.com>
2024-01-08 12:02:24 +0000

    Shape the dog image into a circle

--- app/src/main/java/com/example/jetpackcompose/materialthme/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/materialthme/MainActivity.kt
@@ -30,10 +30,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -108,7 +111,9 @@ fun DogIcon(
     Image(
         modifier = modifier
             .size(64.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
         painter = painterResource(dogIcon),
 
         // Content Description is not needed here - image is decorative, and setting a null content
